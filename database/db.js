e8ef32b801cfd16e67dc5609c1e209e6ef338079
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-06-07 13:50:32 -0700

    built out database

--- database/db.js
+++ database/db.js
@@ -15,51 +15,78 @@ const pool = new Pool({
   port: process.env.dbport,
 });
 
-
 const rawData = [];
-const dataPath = path.join(__dirname, "../../../Downloads/USDA Plants/usdaplants_4-19-2023.csv")
+const dataPath = path.join(__dirname, "../../../Downloads/usdaplants.csv");
+
+const prepareDB = async(pool) => {
+  try {
+    await pool.query("DROP TABLE IF EXISTS usda, comments, zipcode_plant, tags CASCADE")
+    console.log('Dropped all Tables')
+    executeQuery(pool, buildUSDATableCommand, buildZipcode_PlantsTableCommand, buildCommentsTableCommand, buildTagsTableCommand, () => readData(insert)) // all tables are dependent on the USDA table, and once USDA table is built we can also read and then insert the data from starter csv file
+
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const executeQuery = (pool, command, ...cbCommands) => {
+  pool.connect()
+  .then(client => {
+    client.query(command)
+    .then(() => {
+      client.release();
+      console.log('Finished executing:', command);
+      cbCommands.forEach(command => {
+        if (typeof command === "string") {
+          executeQuery(pool, command); // query command
+        } else {
+          command(); // function
+        }
+      })
+    })
+  })
+}
 
 // read data files
-const readData = () => {
-  return (
-    fs.createReadStream(dataPath)
+const readData = (cb) => {
+  return fs.createReadStream(dataPath)
     .pipe(csv())
     .on("data", (row) => {
       rawData.push(row);
     })
     .on("end", () => {
-      console.log(rawData, "FINISHED PARSING")
+      console.log("FINISHED PARSING")
+      cb()
     })
     .on("error" , (err) => {
       console.log(err.message)
     })
-  )
 }
-
-await pool.connect().then((client) => client.query("DROP TABLE IF EXISTS usda, comments, zipcode_plants"))
-
-const createTables = (command) => {
-  return pool.connect()
-  .then(client => {
-    client.query(command)
-    .then(() => {
-      client.release();
-      console.log('Finished executing', command);
-    })
-  })
-}
-
-const buildUSDATableCommand = "CREATE TABLE usda (id SERIAL PRIMARY KEY UNIQUE NOT NULL, scientific_name VARCHAR(100) NOT NULL, common_name VARCHAR(100) NOT NULL)"
-const buildCommentsTableCommand = "CREATE TABLE comments (id SERIAL PRIMARY KEY UNIQUE NOT NULL, comment text NOT NULL, usda_id INT NOT NULL REFERENCES usda(id))"
-const buildZipcode_PlantsTableCommand = "CREATE TABLE tags(id SERIAL PRIMARY KEY UNIQUE NOT NULL, tags VARCHAR(80) NOT NULL, usda_id INT REFERENCES usda(id) NOT NULL, zipCode VARCHAR(5) NOT NULL)"
-
 
 // extract relevant data and insert to postgres
 const insert = () => {
-  rawData.forEach((plant) => {
-    pool.connect()
-    .then(client => {
-      client.query('INSERT INTO ')
-    })
+  console.log('inserting to database');
+  let insertPromises = rawData.map((plant) => new Promise((resolve, reject) =>
+      pool.connect().then(client => {
+        client.query('INSERT INTO usda(id, scientific_name, common_name) VALUES($1, $2, $3);', [plant["betydb.species.id"], plant.ScientificName, plant.CommonName])
+        .then(() => {
+          client.release();
+          resolve();
+        })
+        .catch((err) => reject(err));
+      })
+    )
+  )
+  Promise.all(insertPromises).then(() => {
+    console.log('Finished inserting to databases')
   })
 }
+const buildUSDATableCommand = "CREATE TABLE usda (id SERIAL PRIMARY KEY UNIQUE NOT NULL, scientific_name VARCHAR(100) NOT NULL, common_name VARCHAR(100) NOT NULL)";
+
+const buildZipcode_PlantsTableCommand = "CREATE TABLE zipcode_plant(id SERIAL PRIMARY KEY UNIQUE NOT NULL, submission_count int NOT NULL DEFAULT 1, usda_id INT REFERENCES usda(id) NOT NULL, zipCode VARCHAR(5) NOT NULL)";
+
+const buildCommentsTableCommand = "CREATE TABLE comments (id SERIAL PRIMARY KEY UNIQUE NOT NULL, comment text NOT NULL, zipcode_plant_id INT NOT NULL REFERENCES zipcode_plant(id))";
+
+const buildTagsTableCommand = "CREATE TABLE tags(id SERIAL PRIMARY KEY UNIQUE NOT NULL, characteristic VARCHAR(50) NOT NULL, zipcode_id INT REFERENCES zipcode_plant(id) NOT NULL)"
+
+prepareDB(pool)
